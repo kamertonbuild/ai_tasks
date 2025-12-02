865eebf3c98d4553de8d7879fe5c7e369be9a620
kamertonbuild <kamertonbuild2007@yandex.ru>
2025-12-03 01:10:05 +0300

    Test tasks: 1. Humidifier 2. Maximization of the number

---
+++ src/main/java/ru/yandex/ai/MaxDigital.java
@@ -0,0 +1,31 @@
+package ru.yandex.ai;
+
+import java.util.Arrays;
+
+/**
+ * Задача 2.
+ * Даны два строковых представления чисел A и B. Нужно максимизировать A, заменив в нём любую
+ * цифру на цифру из B. Каждую цифру B можно использовать только один раз.
+ */
+public class MaxDigital {
+
+    public String maximizedDigitalA(String a, String b) {
+        char[] bArray = b.toCharArray();
+        Arrays.sort(bArray);
+
+        int aLength = a.length();
+        StringBuilder result = new StringBuilder();
+        int ai;
+        int bi;
+
+        for (ai = 0, bi = bArray.length - 1; ai < aLength && bi >= 0; ai++, bi--) {
+            result.append((a.charAt(ai) > bArray[bi]) ? a.charAt(ai) : bArray[bi]);
+        }
+
+        if (aLength > bArray.length) {
+            result.append(a.substring(ai));
+        }
+
+        return result.toString();
+    }
+}
---
+++ src/test/java/ru/yandex/ai/MaxDigitalTest.java
@@ -0,0 +1,31 @@
+package ru.yandex.ai;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * Тест для {@link MaxDigital}.
+ */
+class MaxDigitalTest {
+
+    MaxDigital maxDigital = new MaxDigital();
+
+    @ParameterizedTest
+    @CsvSource({
+        "\"\",\"\",\"\"",
+        "0, 0, 0",
+        "0, 279, 9",
+        "098, 100, 198",
+        "2345, 1, 2345",
+        "2345, 0, 2345",
+        "2345, 193, 9345",
+        "2345, 1583, 8545",
+        "2345, 15839, 9855",
+    })
+    void maximizedDigitalA(String a, String b, String result) {
+        assertThat(maxDigital.maximizedDigitalA(a, b))
+            .isEqualTo(result);
+    }
+}
